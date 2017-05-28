83ff33534d272961216e89fba630c2ed1f84e7b4
Enjoyed <g.fleque@gmail.com>
2017-05-28 13:54:07 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# Set the command we want to check
 SERVICE='php -q /opt/websocket/bin/server.php &'
+# Si existeix, executa
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
+    # Per actualitzar les dades comprobant els arduinos.
     nodejs /opt/websocket/refresh.js
 fi
