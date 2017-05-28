ceed87a153afc0818c4ffbf89fb48dcf6bf2a293
Enjoyed <g.fleque@gmail.com>
2017-05-28 14:03:01 +0200

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,5 +1,12 @@
 <?php
+//
+// NOTE: This is a trust=yes web, any query will affect the whole system.
+// DONT DO IT MANUALLY. Only thinked to process Arduino queries.
+//
+
+// Get the IP of the client connecting
 $connecting_as = $_SERVER['REMOTE_ADDR'];
+// List of accepted IPs
 $accepted = array(
 	"::1",
 	"127.0.0.1",
@@ -9,21 +16,31 @@ $accepted = array(
 	"10.19.250.40",
 	"10.19.250.50"
 );
+// Check if it is valid
 if(in_array($connecting_as, $accepted))
 {
+	// Check if there are GET requests
 	if(count($_GET))
 	{
+		// Get GET request
 		$query = $_SERVER['QUERY_STRING'];
+		// Divide it in small piecessss
 		$send = explode('&', $query);
+		// Default command (to update)
 		$command = "nodejs /opt/websocket/updater.js ";
+		// Foreach query value
 		foreach($send as $valor){
+			// Add it to the command as a parameter
 			$command .= "$valor ";
 		}
+		// Run it in the shell!
 		exec($command);
 	}
 }
+// Non accepted visitors
 else
 {
+	// Say we dont want annoying access.
 	echo "YOU DONT HAVE PERMISSIONS TO ACCESS THIS PAGE.";
 }
 ?>
